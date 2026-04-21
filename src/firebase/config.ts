8876d5cced700e9f829986118ed015c89c9132c0
CREEPPER MOMENT <creeppermoment@gmail.com>
2026-04-21 14:54:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8923344947-b67f0",
+  "appId": "1:1061039702372:web:500f04db28f6289f22ba71",
+  "apiKey": "AIzaSyCULzqcY9O_kDhXsnDaV9_K7xnKscNLv3Q",
+  "authDomain": "studio-8923344947-b67f0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1061039702372"
+};
